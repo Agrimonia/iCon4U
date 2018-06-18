122a610b5d79c72ab7c09a908a51f6587a4c21e6
Yongcong Lin <646677472@qq.com>
2018-06-18 22:45:24 +0800

    item: stateless function => React.Component

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,38 +1,66 @@
 import * as React from 'react';
 import { SwatchColorPicker } from 'office-ui-fabric-react/lib/SwatchColorPicker';
+//import { svgToBase64 } from '../loadsvg';
+import { windows } from '../base64';
+interface ItemProps {
+  item: string,
+  index: number
+}
+export default class Item extends React.Component<ItemProps> {
+  constructor(props) {
+    super(props);
+  }
+  // test
+  svgToBase64 = () => {
+    return windows;
+  }
+  click = async () => {
+    console.log('click');
+    Office.context.document.setSelectedDataAsync(this.svgToBase64(), {
+      coercionType: Office.CoercionType.Image,
+      imageWidth: 200
+    }, (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+        console.log(asyncResult.error.message);
+      }
+    });
+  }
 
-export default function Item(item, index) {
-  return (
-    <div key={index} className='ms-ListGhostingExample-container' data-is-scrollable={true}>
-      <div className='IconBar'>
-        <button id='iconlogo' onClick={this.click}>
-          <div className='icon-svg'>
-            <div dangerouslySetInnerHTML={{ __html: item }} />
-          </div>
-        </button>
-        <div id='info'>
-          <div>
-            {/(?:id='fa-)(\S*)/.exec(item)[0].slice(7, -1)}
-          </div>
-          <div id='colorpicker'>
-            <img src='https://localhost:3000/assets/kisspng-primary-color.png' height='35' width='35' />
-          </div>
-          <SwatchColorPicker
-            onCellFocused={
-              (_id, color) => {
-                console.log(color);
+  public render(): JSX.Element {
+    const {item, index} = this.props;
+    return (
+      <div key={index} className='ms-ListGhostingExample-container' data-is-scrollable={true}>
+        <div className='IconBar'>
+          <button id='iconlogo' onClick={this.click}>
+            <div className='icon-svg'>
+              <div dangerouslySetInnerHTML={{ __html: item }} />
+            </div>
+          </button>
+          <div id='info'>
+            <div>
+              {/(?:id='fa-)(\S*)/.exec(item)[0].slice(7, -1)}
+            </div>
+            <div id='colorpicker'>
+              <img src='https://localhost:3000/assets/kisspng-primary-color.png' height='35' width='35' />
+            </div>
+            <SwatchColorPicker
+              onCellFocused={
+                (_id, color) => {
+                  // TODO: setState => click(this.state.color)
+                  console.log(color);
+                }
               }
-            }
-            columnCount={3}
-            cellShape={'circle'}
-            colorCells={[
-              { id: 'b', label: 'Black', color: '#000000' },
-              { id: 'g', label: 'Gray', color: '#eaeaea' },
-              { id: 'w', label: 'White', color: '#ffffff' }
-            ]}
-          />
+              columnCount={3}
+              cellShape={'circle'}
+              colorCells={[
+                { id: 'b', label: 'Black', color: '#000000' },
+                { id: 'g', label: 'Gray', color: '#eaeaea' },
+                { id: 'w', label: 'White', color: '#ffffff' }
+              ]}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-
 import Item from './item';
-// import { svgToBase64 } from '../loadsvg';
-import { windows } from '../base64';
+
 interface IconListProps {
   result: string[];
 }
@@ -11,26 +9,12 @@ export default class ItemList extends React.Component<IconListProps, any> {
   constructor(props) {
     super(props);
   }
-  svgToBase64 = () => {
-    return windows;
-  }
 
-  click = async () => {
-    console.log('click');
-    Office.context.document.setSelectedDataAsync(this.svgToBase64(), {
-      coercionType: Office.CoercionType.Image,
-      imageWidth: 200
-    }, (asyncResult) => {
-        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-          console.log(asyncResult.error.message);
-        }
-      });
-  }
   public render(): JSX.Element {
     const { result } = this.props;
     const ListIcons = result.map((item, index) => {
       return (
-        Item(item, index)
+        <Item item={item} index={index} />
       );
     });
     return (
