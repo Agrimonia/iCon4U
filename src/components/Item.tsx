429f795192c687003e749e7d71b7d9fc63c0cb45
Yongcong Lin <646677472@qq.com>
2018-10-02 15:28:14 +0800

    style: fix icon list display

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -30,24 +30,41 @@ export default class Item extends React.Component<ItemProps> {
 
   public render(): JSX.Element {
     const {item, index} = this.props;
-    return <div key={index} className='ms-ListGhostingExample-container' data-is-scrollable={true}>
+    return (
+      <div key={index} data-is-scrollable={true}>
         <div className='IconBar'>
           <button className='iconlogo' onClick={this.click}>
-          <div className='icon-svg'>
+            <div className='icon-svg'>
               <div dangerouslySetInnerHTML={{ __html: item }} />
             </div>
           </button>
-        <div className='info ms-font-m-plus'>
-            <div>{/(?:id='fa-)(\S*)/.exec(item)[0].slice(7, -1)}</div>
+          <div className='ms-font-m-plus'>
+            <div className='IconName'>{/(?:id='fa-)(\S*)/.exec(item)[0].slice(7, -1)}</div>
             <div className='colorpicker'>
               <img src='https://localhost:3000/assets/kisspng-primary-color.png' height='35' width='35' />
             </div>
-            <SwatchColorPicker onCellFocused={(_id, color) => {
+            <SwatchColorPicker
+              onCellFocused={(_id, color) => {
                 // TODO: setState => click(this.state.color)
                 console.log(color);
-              }} columnCount={3} cellShape={'circle'} colorCells={[{ id: 'b', label: 'Black', color: '#000000' }, { id: 'g', label: 'Gray', color: '#eaeaea' }, { id: 'w', label: 'White', color: '#ffffff' }]} />
+              }}
+              columnCount={3}
+              cellShape={'circle'}
+              colorCells={[
+                {
+                  id: 'b', label: 'Black', color: '#000000'
+                },
+                {
+                  id: 'g', label: 'Gray', color: '#eaeaea'
+                },
+                {
+                  id: 'w', label: 'White', color: '#ffffff'
+                }
+              ]}
+            />
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
